64851b723ece262c17541a4d9409be6f99d22335
Daniel Poles <daniel@dpulm.online>
2021-03-23 23:31:34 +0100

    pointers

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -23,26 +23,24 @@ vector<array<bool, 7>> num = {
 int main()
 {
     array<int, 4> Digits = {22, 27, 17, 24};
-    vector<reference_wrapper<DigitalOut>> digits;
+    vector<DigitalOut*> digits;
 
     array<int, 7> Segments = {11, 4, 23, 8, 7, 10, 18};
-    vector<reference_wrapper<DigitalOut>> segments;
+    vector<DigitalOut*> segments;
 
     for (int i = 0; i < Digits.size(); i++)
     {
-        DigitalOut out(Digits[i]);
-        digits.push_back(out);
-        digits[i].get().off();
+        digits.push_back(new DigitalOut (Digits[i]));
+        digits[i]->off();
     }
     for (int i = 0; i < Segments.size(); i++)
     {
-        DigitalOut out(Segments[i]);
-        segments.push_back(out);
-        segments[i].get().off();
+        segments.push_back(new DigitalOut(Segments[i]));
+        segments[i]->off();
     }
 
-    segments[1].get().on();
-    segments[2].get().on();
+    segments[1]->on();
+    segments[2]->on();
     std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 
     // std::for_each(Digits.begin(), Digits.end(), [](int n) {
@@ -56,23 +54,23 @@ int main()
     int n[] = {1, 2, 3, 4};
     bool stop = false;
 
-    while (!stop)
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            // proms.push(digits[i].write(0));
-            for (int j = 0; j < 7; j++)
-            {
-                if (num[n[i]][j])
-                    segments[j].on();
-                else
-                    segments[j].off();
-            }
-            digits[i].off();
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            digits[i].on();
-        }
-    }
+    // while (!stop)
+    // {
+    //     for (int i = 0; i < 4; i++)
+    //     {
+    //         // proms.push(digits[i].write(0));
+    //         for (int j = 0; j < 7; j++)
+    //         {
+    //             if (num[n[i]][j])
+    //                 segments[j].on();
+    //             else
+    //                 segments[j].off();
+    //         }
+    //         digits[i].off();
+    //         std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    //         digits[i].on();
+    //     }
+    // }
 
     return 0;
 }
